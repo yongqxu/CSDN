1556be5cd9c26839f684b66bd74583c7b8b67312
yongqxu <yongqin_xu@apple.com>
2020-05-23 22:52:46 -0700

    CSDN execises.

--- Numpy_ch1_2arrays_squared.py
+++ Numpy_ch1_2arrays_squared.py
@@ -3,6 +3,7 @@ from numpy import arange
 def sum(n):
     a = arange(n) ** 2 # square for each element in the array
     print(a)
+    print(a.shape[0])
     b = arange(n) ** 4 # power(4) for each element in the array
     print(b)
     return a + b
